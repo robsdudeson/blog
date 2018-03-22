73bb43f5ed7be5994d8dc271b97e0418ba49f2e0
robsdudeson <robsdudeson@gmail.com>
2018-03-21 23:06:16 -0400

    simple blog with articles and comments from http://guides.rubyonrails.org/getting_started.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
+
+  resources :articles do
+    resources :comments
+  end
+
+  root 'welcome#index'
 end
