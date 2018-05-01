e02360863d4d3f21ca46c8144f4e8702c3fe3209
robsdudeson <robsdudeon@gmail.com>
2018-05-01 07:28:03 -0400

    refactor articles.text to articles.body

--- db/migrate/20180322022000_create_articles.rb
+++ db/migrate/20180322022000_create_articles.rb
@@ -2,7 +2,7 @@ class CreateArticles < ActiveRecord::Migration[5.1]
   def change
     create_table :articles do |t|
       t.string :title
-      t.text :text
+      t.text :body
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20180405104823) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.text "text"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
